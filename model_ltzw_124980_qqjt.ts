a4125c4ef7a5c95bed945561df1b2890e593a74d
BezuhlyiYevhen <bezuhlyi.yevhen2@gmail.com>
2024-03-20 16:37:25 +0200

    Added tests

---
+++ model_ltzw_124980_qqjt.ts
@@ -0,0 +1,15 @@
+94 + 40
+const multiply = (a, b) => a * b;
+1 + orange
+const getRandomSubset = (array, size) => array.slice(0, size);
+let result = performOperation(getRandomNumber(), getRandomNumber());
+grape
+
+let result = performOperation(getRandomNumber(), getRandomNumber());
+const truncateString = (str, maxLength) => str.length > maxLength ? `${str.slice(0, maxLength)}...` : str;
+const filterObject = (obj, predicate) => Object.fromEntries(Object.entries(obj).filter(([key, value]) => predicate(value)));
+apple
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+const timeoutPromise = (ms, value) => new Promise(resolve => setTimeout(() => resolve(value), ms));
+const generateUUID = () => `${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}-${Math.random().toString(36).substr(2, 9)}`;
